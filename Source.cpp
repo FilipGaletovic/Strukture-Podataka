e60971059eddb46feeeb0c7f858a00a60587577f
deniskulusic <deniskulusic25@gmail.com>
2024-10-07 19:50:23 +0200

    Prvi zadatak

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#define ERROR 1
+#define MAX 1024
+int NumberOfRows(char FileName);
 
 int main() {
-	printf("Hello world");
+	int number_of_rows = 0;
+	number_of_rows = NumberOfRows("Studenti.txt");
+
+	return 0;
+}
+int NumberOfRows(const char *FileName){
+
+	FILE* file = NULL;
+	file = fopen("", "r");
+	char buffer[MAX];
+	if (file == NULL) {
+		printf("Ne moze se otvoriti file");
+		return ERROR;
+	}
+	while (!feof(file)) {
+		fgets();
+		number_of_rows++;
+	}
 }
