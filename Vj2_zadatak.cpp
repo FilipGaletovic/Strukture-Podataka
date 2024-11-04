738b1418549d03b3fea393f4f791d8d0909de02f
Filip Galetovic <114911410+FilipGaletovic@users.noreply.github.com>
2024-11-04 15:11:03 +0100

    Update Vj2_zadatak.cpp

--- Vj2_zadatak.cpp
+++ Vj2_zadatak.cpp
@@ -1,21 +1,19 @@
-
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #define MAX 1024
+
 struct Osoba;
 typedef struct Osoba* pozicija;
 
-
-typedef struct Osoba
-{
+typedef struct Osoba {
     char ime[MAX];
     char prez[MAX];
     int god;
     pozicija next;
-}osoba;
-
+} osoba;
 
 pozicija kreirajosobu();
 void unospocetak(pozicija);
@@ -26,40 +24,36 @@ void brisi(pozicija, char[MAX]);
 void izbrisilistu(pozicija);
 void dodaj_ispred(pozicija, char[MAX]);
 void dodaj_iza(pozicija, char[MAX]);
+void sort_po_prezimenu(pozicija);
+void upisi_u_datoteku(pozicija, const char*);
+void ucitaj_iz_datoteke(pozicija, const char*);
 
-
-int main()
-{
+int main() {
     int izbor;
     char prezime[MAX];
-    pozicija head;
-    head = (pozicija)malloc(sizeof(osoba));
-
+    pozicija head = (pozicija)malloc(sizeof(osoba));
     if (head == NULL) {
         printf("Error allocating memory \n");
         return -1;
     }
     head->next = NULL;
-    do
-    {
-        printf("\n\n\nIzaberi 1 za dodat novi element na pocetak liste\n");
-        printf("Izaberi 2 za ispis liste\n");
-        printf("Izaberi 3 za dodat novi element na kraj liste\n");
-        printf("Izaberi 4 za pronalazak elementa po prezimenu\n");
 
-        printf("Izaberi 5 za brisanje odredenog elementa\n");
-        printf("Izaberi 6 za dodavanje novog elementa iza određenog elementa\n");
-        printf("Izaberi 7 za dodavanje novog elementa ispred određenog elementa\n");
-        printf("Izaberi 8 za Sortiranje liste po prezimenu\n");
-        printf("Izaberi 9 za upis liste elemenata u datoteku i citanje liste iz iste te datoteke\n");
-        printf("Izaberi 10 za kraj programa\n\n\n\n");
-
-
+    do {
+        printf("\n\nIzaberi opciju:\n");
+        printf("1 - Dodaj novi element na pocetak liste\n");
+        printf("2 - Ispis liste\n");
+        printf("3 - Dodaj novi element na kraj liste\n");
+        printf("4 - Pronadji element po prezimenu\n");
+        printf("5 - Obrisi element\n");
+        printf("6 - Dodaj element iza odredjenog elementa\n");
+        printf("7 - Dodaj element ispred odredjenog elementa\n");
+        printf("8 - Sortiraj listu po prezimenu\n");
+        printf("9 - Upisi listu u datoteku i citaj iz nje\n");
+        printf("10 - Kraj programa\n");
 
         scanf("%d", &izbor);
 
-        switch (izbor)
-        {
+        switch (izbor) {
         case 1:
             unospocetak(head);
             break;
@@ -70,170 +64,217 @@ int main()
             unoskraj(head);
             break;
         case 4:
-            {
-                printf("unesi trazeno prezime: \n");
-                scanf("%s", prezime);
-                pozicija trazeno = pretrazi(head, prezime);
-                if (trazeno) {
-                    printf("Ime: %s, Prezime: %s, Godina: %d", trazeno->ime, trazeno->prez, trazeno->god);
-                }
-                else {
-                    printf("Nema elementa");
-                }
+            printf("Unesi prezime: ");
+            scanf("%s", prezime);
+            if (pozicija trazeno = pretrazi(head, prezime)){
+            
+                printf("Ime: %s, Prezime: %s, Godina: %d\n", trazeno->ime, trazeno->prez, trazeno->god);
             }
-                break;
+            else {
+                printf("Nema elementa\n");
+            }
+            break;
         case 5:
-            printf("unesi prezime osobe koju zelis izbrisati \n ");
+            printf("Unesi prezime osobe koju zelis izbrisati: ");
             scanf("%s", prezime);
             brisi(head, prezime);
             break;
         case 6:
+            printf("Unesi prezime osobe iza koje unosimo novu osobu: ");
+            scanf("%s", prezime);
+            dodaj_iza(head, prezime);
+            break;
+        case 7:
             printf("Unesi prezime osobe ispred koje unosimo novu osobu: ");
             scanf("%s", prezime);
             dodaj_ispred(head, prezime);
-        case 7:
-            printf("Unesi prezime osobe iza koje unosimo novu osobu: ");
-            scanf("%s", prezime);   
-            dodaj_iza(head, prezime);
+            break;
+        case 8:
+            sort_po_prezimenu(head);
+            break;
+        case 9:
+            upisi_u_datoteku(head, "osobe.txt");
+            izbrisilistu(head);  // Clear the list before loading
+            ucitaj_iz_datoteke(head, "osobe.txt");
+            break;
         case 10:
+            izbrisilistu(head);
             break;
         default:
-            printf("\tGreska\n");
+            printf("Greska\n");
             break;
         }
     } while (izbor != 10);
 
-    izbrisilistu(head);
-
-
+    free(head);
     return 0;
 }
 
-
-pozicija kreirajosobu()
-{
-    pozicija os = NULL;
-    os = (pozicija)malloc(sizeof(osoba));
+pozicija kreirajosobu() {
+    pozicija os = (pozicija)malloc(sizeof(osoba));
     if (!os) return NULL;
-    printf("unesi ime ");
+    printf("Unesi ime: ");
     scanf("%s", os->ime);
-    printf("unesi prezime ");
+    printf("Unesi prezime: ");
     scanf("%s", os->prez);
-    printf("unesi godinu ");
+    printf("Unesi godinu: ");
     scanf("%d", &os->god);
-
+    os->next = NULL;
     return os;
-
-
 }
 
 void dodaj_ispred(pozicija head, char prezime[MAX]) {
     pozicija nova_osoba = kreirajosobu();
-    pozicija temp = head->next;
-    pozicija trazena = pretrazi(head, prezime);
-    while (temp->next != trazena) {
+    pozicija temp = head;
+    while (temp->next != NULL && strcmp(temp->next->prez, prezime) != 0) {
         temp = temp->next;
     }
-    temp->next = nova_osoba;
-    nova_osoba->next = trazena;
-    
+    if (temp->next != NULL) {
+        nova_osoba->next = temp->next;
+        temp->next = nova_osoba;
+    }
+    else {
+        printf("Nije pronadjen element s prezimenom %s\n", prezime);
+        free(nova_osoba);
+    }
 }
 
 void dodaj_iza(pozicija head, char prezime[MAX]) {
     pozicija nova_osoba = kreirajosobu();
-    pozicija temp = head->next;
     pozicija trazena = pretrazi(head, prezime);
-    while (temp != trazena->next) {
-        temp = temp->next;
+    if (trazena != NULL) {
+        nova_osoba->next = trazena->next;
+        trazena->next = nova_osoba;
     }
-    trazena->next = nova_osoba;
-    nova_osoba->next = temp;
-
+    else {
+        printf("Nije pronadjen element s prezimenom %s\n", prezime);
+        free(nova_osoba);
+    }
 }
-void unospocetak(pozicija head)
-{
-    pozicija novi;
-    pozicija prvi = head->next;
-    novi = kreirajosobu();
+
+void unospocetak(pozicija head) {
+    pozicija novi = kreirajosobu();
+    novi->next = head->next;
     head->next = novi;
-    novi->next = prvi;
-
-
-
 }
 
-void unoskraj(pozicija head)
-{
-    pozicija novi;
-    pozicija zadnji = head->next;
-    while (zadnji->next != NULL)
-    {
+void unoskraj(pozicija head) {
+    pozicija novi = kreirajosobu();
+    pozicija zadnji = head;
+    while (zadnji->next != NULL) {
         zadnji = zadnji->next;
     }
-    novi = kreirajosobu();
-
     zadnji->next = novi;
-    novi->next = NULL;
-
-
-
 }
 
-void ispis(pozicija head)
-{
-    pozicija trenutni = head->next;
-    while (trenutni != NULL)
-    {
-        printf("ime %s prezime %s godina %d \n", trenutni->ime, trenutni->prez, trenutni->god);
-        trenutni = trenutni->next;
+void ispis(pozicija head) {
+    pozicija temp = head->next;
+    while (temp != NULL) {
+        printf("Ime: %s, Prezime: %s, Godina: %d\n", temp->ime, temp->prez, temp->god);
+        temp = temp->next;
     }
 }
-pozicija pretrazi(pozicija head, char prezime[MAX])
-{
-    int a = 0;
-    pozicija temp = head->next;
-    while (temp != NULL)
-    {
 
-        if (strcmp(prezime, temp->prez) == 0)
-        {
-            a = 1;
-            return temp->next;
+pozicija pretrazi(pozicija head, char prezime[MAX]) {
+    pozicija temp = head->next;
+    while (temp != NULL) {
+        if (strcmp(temp->prez, prezime) == 0) {
+            return temp;
         }
         temp = temp->next;
     }
-    if (a == 0)
-        return NULL;
+    return NULL;
 }
-void brisi(pozicija head, char prezime[MAX])
-{
 
-    pozicija prethodni = head;
-    pozicija trenutni = NULL;
-    while (prethodni->next != NULL && strcmp(prethodni->next->prez, prezime))
-    {
-        prethodni = prethodni->next;
-
+void brisi(pozicija head, char prezime[MAX]) {
+    pozicija temp = head;
+    while (temp->next != NULL && strcmp(temp->next->prez, prezime) != 0) {
+        temp = temp->next;
     }
-    trenutni = prethodni->next;
-    if (trenutni == NULL)
-    {
-        printf("nema zeljene osobe\n");
+    if (temp->next != NULL) {
+        pozicija za_brisanje = temp->next;
+        temp->next = za_brisanje->next;
+        free(za_brisanje);
     }
-    else
-    {
-        prethodni->next = trenutni->next;
-        free(trenutni);
-        printf(" \nizbrisana je osoba sa prezimenom %s \n", prezime);
+    else {
+        printf("Nema osobe s prezimenom %s\n", prezime);
     }
 }
-void izbrisilistu(pozicija head)
-{
-    pozicija temp = NULL;
-    while (head->next != NULL)
-    {
+
+void izbrisilistu(pozicija head) {
+    pozicija temp;
+    while (head->next != NULL) {
         temp = head->next;
-        head->next = head->next->next;
+        head->next = temp->next;
         free(temp);
     }
+}
+
+void sort_po_prezimenu(pozicija head) {
+    pozicija i, j, prev_i, prev_j, temp;
+    for (i = head->next; i != NULL && i->next != NULL; i = i->next) {
+        for (j = i->next; j != NULL; j = j->next) {
+            if (strcmp(i->prez, j->prez) > 0) {
+                char temp_ime[MAX], temp_prez[MAX];
+                int temp_god;
+                strcpy(temp_ime, i->ime);
+                strcpy(temp_prez, i->prez);
+                temp_god = i->god;
+
+                strcpy(i->ime, j->ime);
+                strcpy(i->prez, j->prez);
+                i->god = j->god;
+
+                strcpy(j->ime, temp_ime);
+                strcpy(j->prez, temp_prez);
+                j->god = temp_god;
+            }
+        }
+    }
+}
+
+void upisi_u_datoteku(pozicija head, const char* filename) {
+    FILE* fp = fopen(filename, "w");
+    if (!fp) {
+        printf("Greska pri otvaranju datoteke\n");
+        return;
+    }
+    pozicija temp = head->next;
+    while (temp) {
+        fprintf(fp, "%s %s %d\n", temp->ime, temp->prez, temp->god);
+        temp = temp->next;
+    }
+    // Zatvaramo datoteku
+    fclose(fp);
+}
+
+void ucitaj_iz_datoteke(pozicija head, const char* filename) {
+    FILE* fp = fopen(filename, "r");
+    if (!fp) {
+        printf("Greska pri otvaranju datoteke\n");
+        return;
+    }
+
+    // Pozicioniramo se na kraj liste
+    pozicija current = head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+
+    char ime[MAX], prez[MAX];
+    int god;
+    while (fscanf(fp, "%s %s %d", ime, prez, &god) == 3) {
+        // Kreiramo novi čvor
+        pozicija nova_osoba = (pozicija)malloc(sizeof(osoba));
+        strcpy(nova_osoba->ime, ime);
+        strcpy(nova_osoba->prez, prez);
+        nova_osoba->god = god;
+        nova_osoba->next = NULL;
+
+        // Dodajemo novi čvor na kraj liste
+        current->next = nova_osoba;
+        current = current->next;
+    }
+
+    fclose(fp);
 }
